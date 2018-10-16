074c0aa9631597f3965c0c2f53c41adddd5a8551
unknown <grimoargm@gmail.com>
2018-10-16 12:21:45 +0200

    dodat wall

---
+++ wall.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/wall.css">
+    
+</head>
+<body>
+    <?php include "navbar.php"?>
+
+<div class="pageContainer"> 
+<div class="leftColumn"> 
+    <img class="profPicture" src="img/petprofilepic.png"> 
+    <br>
+    <br>
+    <div class="userInfo">
+        <table>
+            <tr>
+                <td>Full Name</td>
+                <td>John Smith</td>
+            </tr> 
+            <tr>
+                <td>Username</td>
+                <td>Johny</td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>johny@johny.yu</td>
+            </tr>
+        </table> 
+    </div>
+
+<br>
+<br>
+<div class="imgPost">
+<p> placeholder</p>
+<img src="img/female-profile-img.jpg"/>
+</div>
+</div>
+    <div class="rightColumn">
+        <div class="posts">
+            <p>lorem ipsum lipsum trololo</p>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
