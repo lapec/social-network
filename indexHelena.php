823927835511b61cb98d4e0921cd1977bed05bdf
Helena Drinic <drinic_h@outlook.com>
2018-10-16 10:49:09 +0200

    popUpHTML,CSS

---
+++ indexHelena.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="styleHelena.css">
+</head>
+<body>
+	<div class="container" id="popupContainer">
+		<div class="row popupContent">
+			<div class="col-md-8 leftColumn"><img src="ruza.jpg" width="100%"></div>
+			<div class="col-md-4 rightColumn">
+	   			<div id="popupTopBar">
+	    			<span id="closePopup">X</span>
+	  			</div>
+
+	       		<div id="comment" placeholder="Comment">
+	       			<form action="" method="post">
+      					<input type="text" name="komentar" placeholder=" What's on your mind?" autocomplete="off" /><br/>
+				</div>
+
+	       			<button id="submitComment" type="submit">Submit</button>
+
+			</div>
+ 				
+		</div>
+	</div>
+
+</body>
+</html>
