267ac7e44d260c60ad3d79d850919a5ffe41c874
Marija Vuković <marijavukovic.tmf@gmail.com>
2018-10-18 22:23:40 +0200

    Added ajax and loading gif

--- clickableImage.js
+++ clickableImage.js
@@ -1,11 +1,21 @@
 var popupContainer = document.getElementById('popupContainer');
 var photoContainer = document.getElementById('photoContainer');
-var imgClassName = document.getElementsByClassName("clickableImage");
+var imgClassName = document.getElementsByClassName('clickableImage');
 
 var imgClick = function() {
     popupContainer.style.display = 'flex';
     photoContainer.style.backgroundImage = 'url(' + this.src + ')'
-    imgId = this.id;
+    var imgId = this.id;
+    $('#imgComment').html('<img src=img/loading.gif width=40 px>');
+    $.ajax({
+        url: 'demo_test.php?slikaID='+imgId, //iskoristiti ovaj ID za drugi php kako bi se povezali komentari sa slikom
+        success: function(result){
+            $('#imgComment').html(result);
+        },
+        error: function(){
+            $('#imgComment').html("Couldn't load comments.");
+        }
+    });
 };
 for (var i = 0; i < imgClassName.length; i++) {
     imgClassName[i].addEventListener('click', imgClick, false);
@@ -13,4 +23,5 @@ for (var i = 0; i < imgClassName.length; i++) {
 
 document.getElementById('closePopup').addEventListener('click', function() { 
     popupContainer.style.display = 'none';
+    $('#imgComment').html('');
 });
