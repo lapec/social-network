ef62e30a4d3c8b455b167cad588f041feaa1d702
Marija Vuković <marijavukovic.tmf@gmail.com>
2018-10-18 11:04:19 +0200

    Changed function for clickableImg

--- clickableImage.js
+++ clickableImage.js
@@ -1,11 +1,15 @@
 var popupContainer = document.getElementById('popupContainer');
 var photoContainer = document.getElementById('photoContainer');
+var imgClassName = document.getElementsByClassName("clickableImage");
 
-document.getElementById('clickableImage').addEventListener('click', function() { 
+var imgClick = function() {
     popupContainer.style.display = 'flex';
     photoContainer.style.backgroundImage = 'url(' + this.src + ')'
-});
-
+    imgId = this.id;
+};
+for (var i = 0; i < imgClassName.length; i++) {
+    imgClassName[i].addEventListener('click', imgClick, false);
+}
 
 document.getElementById('closePopup').addEventListener('click', function() { 
     popupContainer.style.display = 'none';
