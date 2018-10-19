acdf85794f731d8d1aef6b16529e336a2c379bbf
Helena <drinic_h@outlook.com>
2018-10-19 10:51:11 +0200

    DIV for popUpContainer and CSS

--- dashboard.php
+++ dashboard.php
@@ -38,8 +38,7 @@ $result = $conn->query($sql);
 	<title>social network</title>
 	<link href="css/dashboard.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-  <!-- <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
-  <link rel="stylesheet" type="text/css" href="styleHelena.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<style>
 		form {
 			display: inline;
@@ -79,24 +78,23 @@ $result = $conn->query($sql);
     // zatvaramo konekciju ka bazi
     $conn->close();
 ?>
-<div class="container" id="popupContainer">
-    <div class="row popupContent">
-      <div class="col-md-8 leftColumn"><img src="ruza.jpg" width="100%"></div>
-      <div class="col-md-4 rightColumn">
-          <div id="popupTopBar">
-            <span id="closePopup">X</span>
-          </div>
 
-            <div id="comment" placeholder="Comment">
-              <form action="" method="post">
-                <input type="text" name="komentar" placeholder=" What's on your mind?" autocomplete="off" /><br/>
-        </div>
 
-              <button id="submitComment" type="submit">Submit</button>
-
+<div id="popupContainer">
+  <div id="popupContent">
+    <div class="leftColumn" id="photoContainer"></div>
+    <div class="rightColumn">
+      <div id="popupTopBar">
+        <span id="closePopup">X</span>
       </div>
-        
+      <div id="komentar">bla</div>
+      <div id="comment" placeholder="Comment">
+        <form action="" method="post">
+        <input id="forma" type="text" name="komentar" placeholder=" What's on your mind?" autocomplete="off" /><br/>
+      </div>
+      <button id="submitComment" type="submit">Submit</button>
     </div>
   </div>
+</div>
 </body>
 </html>
