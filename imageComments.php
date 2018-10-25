47fc8ee8478fbd444e3623f2a45d58c5e61f7064
BojanVuk <vbojan2@gmail.com>
2018-10-25 10:42:45 +0200

    FIX: Replaced files

--- imageComments.php
+++ imageComments.php
@@ -1,42 +1,33 @@
-<?php session_start();?>
 <?php require "config/db_config.php"; ?>
 
 
 <?php
+
 $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 
 	
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-		$ok = true;
-		$posttext = mysqli_real_escape_string($conn, $_POST['postComment']);
-		$slikaID = mysqli_real_escape_string($conn, $_POST['slikaID']); 
-		if ($ok == "true") {
-			$posttime = date("Y-m-d H:i:s");
-			$currentUserID = $_SESSION['KID'];
-			$sql = "INSERT INTO `komentari_slike` (SID, KID, Komentar, VremePostavljanja) VALUES ('".$slikaID."','".$currentUserID."', '".$posttext."','".$posttime."')";
-			if ($conn->query($sql) !== true) {
-				echo "Error: " . $sql . "<br>" . $conn->error . "<br><br>";
-			} else {
-				$sql = "SELECT * FROM komentari_slike INNER JOIN korisnici ON komentari_slike.KID = korisnici.KID WHERE komentari_slike.SID = '".$slikaID."' ORDER BY VremePostavljanja DESC LIMIT 1";
-				$result = $conn->query($sql);
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) { ?>
-						<div align="center" id="printText">
-							<div class="row" id= "imgComm">
-								<img src="img/<?php echo $row["SlikaKorisnika"] ?>">
-								<span id="fullName"><a href="wall.php?n=<?php echo $row["KID"] ?>"><?php echo htmlspecialchars($row["Ime"]." ".$row["Prezime"]) ?></a></span>
-								<div class="postTxt" id="postTxt"><?php echo htmlspecialchars($row["Komentar"]) ?></div>
-								<div class="dateTime"><?php echo htmlspecialchars($row["VremePostavljanja"]) ?></div>
-								<br><div id="like"><pre>Like    Comment</pre></div>
-							</div>
-						</div>
-					<?php	}
-				} else {
-					echo "No comments yet...";
-				}
-			}
-		}
+
+$slikaID = $_GET['slikaID'];
+$sql = "SELECT * FROM komentari_slike INNER JOIN korisnici ON komentari_slike.KID = korisnici.KID WHERE komentari_slike.SID = '".$slikaID."' ORDER BY VremePostavljanja ASC";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) { ?>
+			<div align="center" id="printText">
+				<div class="row" id= "imgComm">
+					<img src="img/<?php echo $row["SlikaKorisnika"] ?>">
+					<span id="fullName"><a href="wall.php?n=<?php echo $row["KID"] ?>"><?php echo htmlspecialchars($row["Ime"]." ".$row["Prezime"]) ?></a></span>
+					<div class="postTxt" id="postTxt"><?php echo htmlspecialchars($row["Komentar"]) ?></div>
+					<div class="dateTime"><?php echo htmlspecialchars($row["VremePostavljanja"]) ?></div>
+					<br><div id="like"><pre>Like    Comment</pre></div>
+				</div>
+			</div>
+		<?php	}
+	} else {
+		echo "No comments yet...";
+	}
 	$conn->close();
+
 ?>
