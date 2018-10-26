ec7225b8d660bcf93302045dc87b509fc3990f21
raics <34372358+raics@users.noreply.github.com>
2018-10-26 13:55:38 +0200

    Validacije i dorada html-a

--- profile.php
+++ profile.php
@@ -11,7 +11,7 @@
 	<link href="css/profile.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
 </head>
-<body> 
+<body>
     <?php
     include "sections/navbar.php"; 
     include "config/db_config.php";
@@ -19,24 +19,23 @@
     ?>
 <div class="container">
     <div class="content">
-
     <main>
         <div class="gornjideo">
         <div class="levideo">
-    	<img id src="img/<?php echo $userpic; ?>" width="auto" height="300px" ><br><br>
+    	<img class="userimage" src="img/<?php echo $userpic; ?>">
         <form action="" method="post" enctype="multipart/form-data">
-            <label for="image"><button type="button" class = "dropdownbtn">ODABERITE SLIKU</button></label>
-            <input type="file" name="image" id = "dugme"/>
-            <input class = "dropdownbtn" id = "imagebutton" type="submit" value="UPLOAD" />
+            <input type="file" name="image" id="picker" class="dropdownbtn browsebtn"/>
+            <label for="picker">ODABERITE SLIKU</label>
+            <input class="dropdownbtn imgbutton" type="submit" value="UPLOAD" />
         </form>
         <div class="alert"><?php echo $imgerror; ?></div>
         </div>
             
   <div  class="desnideo">
         <form action="" method="post">
-        <input class="input-field" type="text" name="name" value="<?php echo $name; ?>" autocomplete="off"><br>
-        <input class="input-field" type="text" name="lastname" value="<?php echo $lastname; ?>" autocomplete="off"><br>
-        <input class="input-field" type="text" name="username" value="<?php echo $username; ?>" autocomplete="off"><br>
+        <input class="input-field" type="text" name="name" value="<?php echo htmlspecialchars($name); ?>" autocomplete="off">
+        <input class="input-field" type="text" name="lastname" value="<?php echo htmlspecialchars($lastname); ?>" autocomplete="off">
+        <input class="input-field" type="text" name="username" value="<?php echo htmlspecialchars($username); ?>" autocomplete="off">
         <input type="submit" name="updateuserinfo" value = "UPDATE" class = "dropdownbtn">
     </form>
     <div class="alert"><?php echo $infoerror; ?></div>
@@ -50,9 +49,8 @@
         <br>
         <form action="" method="post">
         <?php writePosts(); ?>
-        <input type="submit" name="changeposts" value = "UPDATE" class = "dropdownbtn">
+        <input type="submit" name="changeposts" value = "UPDATE" class = "dropdownbtn" <?php echo $hideupdatebutton; ?> >
     </form>
-    <div class="alert"><?php echo $posterror; ?></div>
 </div>
 <div id="vl"></div>
 <div class="donjidesnideo">
@@ -60,12 +58,9 @@
     <h4>Promena lozinke</h4>
     <br>
     <form action="" method="post">
-        <input class="input-field" type="text" name="currpass" placeholder="Unesite važeću lozinku" autocomplete="off">
-    <br>
-        <input class="input-field" type="text" name="newpass" placeholder="Unesite novu lozinku" autocomplete="off">
-    <br>
-        <input class="input-field" type="text" name="confirmpass" placeholder="Potvrdite novu lozinku" autocomplete="off">
-    <br>
+        <input class="input-field" type="password" name="currpass" placeholder="Unesite važeću lozinku" autocomplete="off"><br>
+        <input class="input-field" type="password" name="newpass" placeholder="Unesite novu lozinku" autocomplete="off"><br>
+        <input class="input-field" type="password" name="confirmpass" placeholder="Potvrdite novu lozinku" autocomplete="off"><br>
         <input type="submit" name="changepassword" value = "UPDATE" class = "dropdownbtn">
     </form>
     <div class="alert"><?php echo $passerror; ?></div>
